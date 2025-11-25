4e039d3cc8c10f2fd543dcde128d9cca0dc4b79a
Tahmid L <103884318+tahlas@users.noreply.github.com>
2025-11-25 12:02:31 +0100

    Removed testing stuff

--- src/index.jsx
+++ src/index.jsx
@@ -2,37 +2,3 @@
 import {createRoot} from "react-dom/client";
 const mountedApp= createRoot(document.getElementById('root'))
 mountedApp.render(<div>Hello World!</div>);
-
-//this is for testing. remove everything below later:
-import "/src/style.css";
-mountedApp.render(<OverviewView />);  
-
-
-function OverviewView(){
-    const tasks = [
-        {title: "Task 1", description: "Description 1"},
-        {title: "Task 2", description: "Description 2"},
-        {title: "Task 3", description: "Description 3"}
-    ];
-
-    return(
-        <div>
-            <table>
-                <tbody>
-                    {tasks.map(tasksRowCB)}
-                </tbody>
-            </table>
-        </div>
-    );
-} 
-
-function tasksRowCB(task){
-    return(
-        <tr>
-            <td className="tasks">
-                <div>{task.title}</div>
-                <div>{task.description}</div>
-            </td>
-        </tr>
-    );
-}

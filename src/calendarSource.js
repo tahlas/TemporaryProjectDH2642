86d56d723b504c0c3c23eeccfa97f8efead92b4d
Tahmid L <103884318+tahlas@users.noreply.github.com>
2025-11-25 11:32:38 +0100

    Calendar Source used for API

---
+++ src/calendarSource.js
@@ -0,0 +1,8 @@
+function getCalendarFromGoogle(){
+    
+    //return fetch("https://www.googleapis.com/calendar/v3/calendars/" + calendarId);
+}
+
+function exchangeAuthCodeForTokens(authCode){
+
+}

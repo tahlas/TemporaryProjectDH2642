9c7b6f69dbddcfaaa280f2355e2475d768ad8451
Tahmid L <103884318+tahlas@users.noreply.github.com>
2025-11-25 11:58:56 +0100

    Working on overview view, added stuff to index.jsx just for testing, should remove later

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,38 @@
 // TODO make a reactive model (application state), pass it as prop to the components used
 import {createRoot} from "react-dom/client";
 const mountedApp= createRoot(document.getElementById('root'))
-mountedApp.render(<div>hello world!</div>);  
+mountedApp.render(<div>Hello World!</div>);
+
+//this is for testing. remove everything below later:
+import "/src/style.css";
+mountedApp.render(<OverviewView />);  
+
+
+function OverviewView(){
+    const tasks = [
+        {title: "Task 1", description: "Description 1"},
+        {title: "Task 2", description: "Description 2"},
+        {title: "Task 3", description: "Description 3"}
+    ];
+
+    return(
+        <div>
+            <table>
+                <tbody>
+                    {tasks.map(tasksRowCB)}
+                </tbody>
+            </table>
+        </div>
+    );
+} 
+
+function tasksRowCB(task){
+    return(
+        <tr>
+            <td className="tasks">
+                <div>{task.title}</div>
+                <div>{task.description}</div>
+            </td>
+        </tr>
+    );
+}
--- src/views/overviewView.jsx
+++ src/views/overviewView.jsx
@@ -1,15 +1,32 @@
 /**
- * Renders the 
- * @returns 
+ * Renders the overview view
+ * @returns the overview view component
  */
 function OverviewView(){
+    const tasks = [
+        {title: "Task 1", description: "Description 1"},
+        {title: "Task 2", description: "Description 2"},
+        {title: "Task 3", description: "Description 3"}
+    ];
+
     return(
         <div>
             <table>
                 <tbody>
-                    
+                    {tasks.map(tasksRowCB)}
                 </tbody>
             </table>
         </div>
-    )
+    );
 } 
+
+function tasksRowCB(task){
+    return(
+        <tr>
+            <td>
+                <div>{task.title}</div>
+                <div>{task.description}</div>
+            </td>
+        </tr>
+    );
+}
